df745c2f4ce66251da1879e561d8690851a87612
jonjau <jonjau3Q@gmail.com>
2018-10-15 20:46:36 +0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import find_packages, setup
 setup(
     name = 'cifero',
     version = '1.0',
-    url = 'https://github.com/mypackage.git',
+    url = 'https://github.com/jonjau/cifero',
     author = 'Jonathan J',
     author_email = 'jonjau3Q@gmail.com',
-    description = 'A personal Python GUI program for phonetic transliteration/encryption',
+    description = 'A personal Python GUI program for phonetic transliteration.',
     license='MIT',
     packages = find_packages(),
     install_requires = ['PyQt5', 'pyperclip'],
